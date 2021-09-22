5947958194e0c2f1b53d73cb8a06e6f71281914a
wimbaertvlotbe <36698691+wimbaertvlotbe@users.noreply.github.com>
2021-09-22 09:11:17 +0200

    Add project files.

---
+++ Thema3_Klassen_en_Objecten_Opdracht4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Thema3_Klassen_en_Objecten_Opdracht4
+{
+    /* OPDRACHT 4 : Methods in een klasse
+
+        1.  Elke klasse moet zichzelf kunnen 'voorstellen'.
+            Hiervoor wordt de ToString-method gebruikt.
+
+            Elke klasse erft al een ToString()-method van de klasse Object (=INHERITANCE).
+
+            - Roep voor een object van de klasse Balk deze ToString-method aan. 
+              Print het resultaat naar de console. Wat stel je vast?  
+            - Zoek op hoe je de ToString-method kan 'overriden' en programmeer in de klasse Balk
+              je eigen ToString-method. Die moet, correct geformatteerd, alle gegevens van de 
+              balk als string retourneren.
+        
+         2. Een Balk heeft een volume. Je gaat een method schrijven om het volume te berekenen. 
+
+            - Programmeer een method Volume waarmee je het volume van de balk kan laten berekenen.
+
+         3. Een balk heeft een massa. je gaat de klasse uitbreiden om de massa te berekenen.
+
+            - Breid de code uit zodat de klasse ook de massa van de balk kan berekenen. Voeg
+              indien nodig extra attributen toe.
+
+         4. Kan je van 'massa' en 'volume' ook klassen/objecten maken? Waarom wel of waarom niet?
+
+    */
+    class Program
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
